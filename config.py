6d92cd1e334e0b810d982c0cf23afd6c89020b08
H Lewin Jesudhas <lewinjesudhas1@gmail.com>
2025-02-09 22:21:18 +0530

    supabase

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from supabase import create_client
 import os
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_ANON_KEY")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
 # Initialize Supabase Client
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
